05af815b80402e277a6da0728caed7180b171ab3
kacperrutkowski <kcprutkowski@gmail.com>
2026-01-28 18:42:15 +0100

    Added variable STREET_NAMES

--- src/poker_enviroment/poker_env.py
+++ src/poker_enviroment/poker_env.py
@@ -7,17 +7,6 @@ SMALL_BLIND = 5
 BIG_BLIND = 10
 MIN_RAISE = BIG_BLIND
 
-ACTION_FOLD = 0
-ACTION_CALL = 1
-ACTION_RAISE = 2
-ACTION_ALL_IN = 3
-
-ACTION_NAMES = {
-    0: "fold",
-    1: "call/check",
-    2: "raise",
-    3: "all-in"
-}
 
 def create_deck() -> list[str]:
     return [r + s for r, s in itertools.product(RANKS, SUITS)]
@@ -42,6 +31,19 @@ class Player:
         self.bet = 0
         self.all_in = False
         self.street_bet = 0
+
+ACTION_FOLD = 0
+ACTION_CALL = 1
+ACTION_RAISE = 2
+ACTION_ALL_IN = 3
+
+ACTION_NAMES = {
+    0: "fold",
+    1: "call/check",
+    2: "raise",
+    3: "all-in"
+}
+
 
 def get_legal_actions(player, to_call) -> list[str]:
 
@@ -194,6 +196,15 @@ FLOP = 1
 TURN = 2
 RIVER = 3
 SHOWDOWN = 4
+
+STREET_NAMES = {
+    0 : "PREFLOP",
+    1 : "FLOP",
+    2 : "TURN",
+    3 : "RIVER",
+    4 : "SHOWDOWN",
+}
+
 
 class PokerEnv:
     def __init__(self, stack_size=1000):
@@ -351,11 +362,3 @@ class PokerEnv:
         }
 
 
-def main():
-    env = PokerEnv()
-    obs = env.reset()
-
-    
-
-if __name__ == "__main__":
-    main()
