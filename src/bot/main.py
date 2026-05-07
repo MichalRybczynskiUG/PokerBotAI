fcb922e4234a27ff0c1af70257f10e8437f8c41f
MichalRybczynskiUG <michal.rybczynski02@gmail.com>
2026-05-07 11:00:16 +0200

    updated main.py

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,7 +1,12 @@
 from trainer import NFSPTrainer
+import os
 
-STATE_DIM = 20
+os.makedirs("checkpoints", exist_ok=True)
+os.makedirs("models", exist_ok=True)
+
+STATE_DIM = 414
 NUM_ACTIONS = 8
 
 trainer = NFSPTrainer(STATE_DIM, NUM_ACTIONS)
-trainer.train(episodes=25000)
+
+trainer.train(episodes=250100)
